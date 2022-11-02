7481acd1f999bf0ba330c15c95ec77fa7fb1a4bc
Lagon <109609208+codeslagon@users.noreply.github.com>
2022-11-02 18:33:45 +0300

    Update Configs.py

--- Configs.py
+++ Configs.py
@@ -3,6 +3,6 @@
 import os
 
 class Config(object):
-    APP_ID = int(os.environ.get("APP_ID", 6))
-    API_HASH = os.environ.get("API_HASH", None)
-    TOKEN = os.environ.get("TOKEN", None)
+    APP_ID = int(os.environ.get("APP_ID", "29569563"))
+    API_HASH = os.environ.get("API_HASH", "d4df9f3d9c24a5292d5cf5a514d11a7a")
+    TOKEN = os.environ.get("TOKEN", "5651459943:AAF40UHwo7zyGDiP5fU208rU8lM0IdtggAo")
